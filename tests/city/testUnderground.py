a39466c398916d87647aebb1bb2375da354183d5
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-06-20 07:17:34 +0100

    empty city.underground module added

---
+++ tests/city/testUnderground.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from city import underground
+import tests.common
+from collections import OrderedDict
+
+
+class networkTests(tests.common.common):
+	pass
