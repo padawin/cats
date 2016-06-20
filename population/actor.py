380b40ddb305c8570e60da61b7686f0f9b960120
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-06-20 10:02:37 +0100

    empty actor module added

---
+++ population/actor.py
@@ -0,0 +1,10 @@
+class actor(object):
+	pass
+
+
+class cat(actor):
+	pass
+
+
+class human(actor):
+	pass
---
+++ tests/population/testActor.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from population import actor
+import tests.common
+from collections import OrderedDict
+
+
+class actorTests(tests.common.common):
+	pass
